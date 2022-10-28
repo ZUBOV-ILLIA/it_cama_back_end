eeaf16a6f6bfac317d24ecb8ea58a0c9f0968de6
ZUBOV ILLIA <zubovillia@gmail.com>
2022-10-28 16:29:32 +0300

    lgtm, need to do more

--- src/server.js
+++ src/server.js
@@ -1,47 +1,69 @@
 const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer((request, response) => {
-    switch (request.url) {
-      case '/home': {
-        fs.readFile('./src/pages/home.html', (err, data) => {
-          if (err) {
-            response.write('500 some error occured');
-            response.end();
-          } else {
-            console.log(data);
-            response.write(data);
-            response.end();
-          }
-        });
-        break;
+const delay = (ms) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, ms)
+  });
+}
+
+const readFile = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
       }
+    })
+  })
+}
 
-      case '/about': {
-        fs.readFile('./src/pages/about.html', (err, data) => {
-          if (err) {
-            response.write('500 some error occured');
-            response.end();
-          } else {
-            response.write(data);
-            response.end();
-          }
-        });
-
-        break;
-      }
-
-      case '/favicon.ico': {
-        const data = fs.readFileSync('./src/favicon.ico');
+const server = http.createServer(async (request, response) => {
+  switch (request.url) {
+    case '/home': {
+      try {
+        const data = await readFile('./src/pages/home.html')
         response.write(data);
         response.end();
-        break;
-      }
-        
-      default:
-        response.write('404 not found');
+      } catch (error) {
+        const data = fs.readFileSync('./src/img.jpeg');
+        response.write(data);
         response.end();
+      }
+
+      break;
     }
+
+    case '/about': {
+      try {
+        await delay(3000);
+        const data = await readFile('./src/pages/aboutt.html')
+        response.write(data);
+        // response.write('ABOUT COURES');
+        response.end();
+      } catch (error) {
+        const data = fs.readFileSync('./src/img.jpeg');
+        response.write(data);
+        response.end();
+      }
+
+      break;
+    }
+
+    case '/favicon.ico': {
+      const data = fs.readFileSync('./src/favicon.ico');
+      response.write(data);
+      response.end();
+      break;
+    }
+
+    default:
+      response.write('404 not found');
+      response.end();
+  }
 
 });
 
