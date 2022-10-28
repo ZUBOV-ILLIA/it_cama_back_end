4edb7f5e44519b22b4ecf523e06a5f2c5ce9f40a
ZUBOV ILLIA <zubovillia@gmail.com>
2022-10-28 15:05:12 +0300

    lgtm

--- src/server.js
+++ src/server.js
@@ -3,15 +3,46 @@ const fs = require('fs');
 
 const server = http.createServer((request, response) => {
     switch (request.url) {
-      case '/home':
-        const data = 'best free online course';
-        response.write(data);
+      case '/home': {
+        fs.readFile('./src/pages/home.html', (err, data) => {
+          if (err) {
+            response.write('500 some error occured');
+            response.end();
+          } else {
+            console.log(data);
+            response.write(data);
+            response.end();
+          }
+        });
         break;
+      }
+
+      case '/about': {
+        fs.readFile('./src/pages/about.html', (err, data) => {
+          if (err) {
+            response.write('500 some error occured');
+            response.end();
+          } else {
+            response.write(data);
+            response.end();
+          }
+        });
+
+        break;
+      }
+
+      case '/favicon.ico': {
+        const data = fs.readFileSync('./src/favicon.ico');
+        response.write(data);
+        response.end();
+        break;
+      }
+        
       default:
         response.write('404 not found');
+        response.end();
     }
 
-    response.end();
 });
 
 server.listen(3003);
