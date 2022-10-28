8ffd9895da31c842e9f320979afb97b65f52caa3
ZUBOV ILLIA <zubovillia@gmail.com>
2022-10-28 12:38:12 +0300

    begining of fs

--- src/server.js
+++ src/server.js
@@ -1,23 +1,17 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((request, response) => {
-  switch (request.url) {
-    case '/home':{
-      const start = new Date();
-      while(new Date() - start < 3000) {
-        console.log(new Date() - start)
-      }
-      const data = 'best free online course';
-      response.write(data);
-      response.end();
-      break;
+    switch (request.url) {
+      case '/home':
+        const data = 'best free online course';
+        response.write(data);
+        break;
+      default:
+        response.write('404 not found');
     }
-    default: {
-      response.write('404 not found');
 
-      response.end;
-    }
-  }
+    response.end();
 });
 
 server.listen(3003);
